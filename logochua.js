5aa10b5acec8dc46d0f6df8f28e6856b9521e895
katharger <57004410+katharger@users.noreply.github.com>
2019-10-25 09:58:21 -0300

    Update logochua.js

--- logochua.js
+++ logochua.js
@@ -1,5 +1,3 @@
-jQuery(window).load(function() {
-    jQuery( ".header--home .header__image.header__image--store" ).remove();
-    jQuery( ".header--home .header__link.header__link--logo" ).append( "<picture class=\"header__image header__image--store\"><img src=\"https:/\/images.tcdn.com.br/709680/themes/5/img/layout/logochuabranca.png\" alt=\"Logo da Chuá Brand\"></picture>" );
-    console.log("Bem vindo a Chuá Brand");
-});
+jQuery( ".header--home .header__image.header__image--store" ).remove();
+jQuery( ".header--home .header__link.header__link--logo" ).append( "<picture class=\"header__image header__image--store\"><img src=\"https:/\/images.tcdn.com.br/709680/themes/5/img/layout/logochuabranca.png\" alt=\"Logo da Chuá Brand\"></picture>" );
+console.log("Bem vindo a Chuá Brand");
