5e9c333551d0094be49996b76d60a66b0ec4c85a
katharger <57004410+katharger@users.noreply.github.com>
2019-10-25 09:53:54 -0300

    Add files via upload

---
+++ logochua.js
@@ -0,0 +1,5 @@
+jQuery(window).load(function() {
+    jQuery( ".header--home .header__image.header__image--store" ).remove();
+    jQuery( ".header--home .header__link.header__link--logo" ).append( "<picture class=\"header__image header__image--store\"><img src=\"https:/\/images.tcdn.com.br/709680/themes/5/img/layout/logochuabranca.png\" alt=\"Logo da Chuá Brand\"></picture>" );
+    console.log("Bem vindo a Chuá Brand");
+});
